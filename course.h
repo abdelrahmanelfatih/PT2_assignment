5aa5bdb5317715df0ad95b5a37cd4ac89150ab89
eilio <167160851+abdelrahmanelfatih@users.noreply.github.com>
2024-10-22 20:47:52 +0800

    last touch

--- course.h
+++ course.h
@@ -1,19 +1,23 @@
 #ifndef COURSE_H
 #define COURSE_H
-#include <string>
-
 #endif //COURSE_H
 
+#include <string>
+
+
 class Course {
-private:
+    //class members are private by default
     std::string name , code;
     double score;
 
 public:
+    //constructor and destructor block
     Course(std::string name, std::string code, double score)
-    : name(name), code(code), score(score) {}
-    Course() : name{} , code{} , score{} {}
+    : name(name), code(code), score(score) {} //sets the variables to the attributes in the class
+    Course() : name{} , code{} , score{} {} //resets the variables and makes them empty
     ~Course(){}
+
+    //methods declaration
     void setName(std::string name);
     void setCode(std::string code);
     void setScore(double score);
