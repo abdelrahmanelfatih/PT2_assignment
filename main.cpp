2bbcb8891f8cad785a0b15bf364035d47032d332
eilio <167160851+abdelrahmanelfatih@users.noreply.github.com>
2024-10-22 20:47:23 +0800

    last touch main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 #include "course.h"
 
 int main() {
-    Course course;
     std::string courseName, courseCode;
     double score;
     int creditHour;
 
+    //user input code block
     std::cout << "Enter the following data: " << std::endl;
 
     std::cout << "Subject name => ";
@@ -18,16 +18,28 @@ int main() {
     std::getline(std::cin, courseCode);
 
     std::cout << std::endl << "Score earned => ";
-    std::cin >> score;
+    if (!(std::cin >> score) || score < 0 || score > 100) { //takes score as input while checking if score is less than 0 or more than 100
+        std::cerr << "Invalid input." << std::endl;
+        return 1;
+    } //this code block takes in score as input while also handling potential errors
 
-    // Cute lil error handling
+    Course course(courseName, courseCode, score);
+
+    // Cute lil error handling as well
     if (!courseCode.empty() && std::isdigit(courseCode.back())) { creditHour = courseCode.back() - '0'; }
-    else { exit(1); }
+    else {
+        std::cerr << "Error : Invalid Course code ";
+        return 1;
+    }
 
-    std::cout <<"\n \n"  << "THE RESULT" << "\n \n"
-        << courseName << std::endl
-        << courseCode << std::endl
-        << creditHour << std::endl
-        << score << std::endl;
+    //user output code block
+    std::cout << "\n THE RESULT \n"
+        << "Subject Code : "  << courseCode << std::endl
+        << "Subject Name : " << courseName << std::endl
+        << "Credit Hour : "  << creditHour << std::endl
+        <<"Score Earned : " << score << std::endl
+         << "Grade Earned : " << course.calculateGrade() << std::endl
+        << "Grade Point : " << course.calculateGradePoint() << std::endl
+         << "Point Earned : " << course.calculatePoint(creditHour) << std::endl;
     return 0;
 }
