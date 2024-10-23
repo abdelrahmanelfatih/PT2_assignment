dbfb89998a9214a6e10478b71f40cfd41d915909
eilio <167160851+abdelrahmanelfatih@users.noreply.github.com>
2024-10-23 16:16:39 +0800

    Update course.h

--- course.h
+++ course.h
@@ -11,13 +11,13 @@ private:
 public:
     // Constructors and destructor
     Course(std::string name, std::string code, double score)
-        : name(std::move(name)), code(std::move(code)), score(score) {}
+        : name(name), code(code), score(score) {}
     Course() : name{}, code{}, score{} {}
     ~Course() {}
 
     // Setters
-    void setName(const std::string& name);
-    void setCode(const std::string& code);
+    void setName(std::string name);
+    void setCode(std::string code);
     void setScore(double score);
 
     // Getters
@@ -26,9 +26,9 @@ public:
     double getScore() const;
 
     // Grade calculation methods
-    std::string calculateGrade() const;
-    double calculateGradePoint() const;
-    double calculatePoint(int creditHour) const;
+    std::string calculateGrade();
+    double calculateGradePoint();
+    double calculatePoint(int creditHour);
 };
 
 #endif //COURSE_H
